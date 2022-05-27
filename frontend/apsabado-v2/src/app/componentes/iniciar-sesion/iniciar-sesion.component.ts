cd46a15394cf99c0c5eaf792326c4b66510a8bb2
gonza7795 <gonza_7795@yahoo.com.ar>
2022-05-27 15:55:20 -0300

    hasta 38:48

--- frontend/apsabado-v2/src/app/componentes/iniciar-sesion/iniciar-sesion.component.ts
+++ frontend/apsabado-v2/src/app/componentes/iniciar-sesion/iniciar-sesion.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AutenticacionService } from 'src/app/servicios/autenticacion.service';
 
 @Component({
   selector: 'app-iniciar-sesion',
@@ -10,7 +12,7 @@ export class IniciarSesionComponent implements OnInit {
 
   form:FormGroup;
   
-  constructor(private formBuilder:FormBuilder) { 
+  constructor(private formBuilder:FormBuilder, private autenticationService:AutenticacionService, private ruta:Router) { 
     this.form=this.formBuilder.group(
       {
         email:["",[Validators.required,Validators.email]],
@@ -35,6 +37,16 @@ export class IniciarSesionComponent implements OnInit {
   get Password() {
 
     return this.form.get('password');
+  }
+
+
+  onEnviar(event:Event)  {
+    event.preventDefault;
+    this.autenticationService.IniciarSesion(this.form.value).subscribe(data=>{
+      console.log("DATA" + JSON.stringify(data));
+      this.ruta.navigate(['portfolio']);
+
+    })
   }
 
 
